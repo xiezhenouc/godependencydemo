0523f48c521823eed78e3265bd167b71743b2552
maxiaotiao <xiezhenouc@163.com>
2019-03-02 22:47:57 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	myframework "github.com/xiezhenouc/golangwebframework"
+)
+
+type TestController struct {
+	Ctx *myframework.Context
+}
+
+func (t *TestController) Init(context *myframework.Context) {
+	t.Ctx = context
+}
+
+func (t *TestController) SayHi() {
+	fmt.Fprintln(t.Ctx.Output, "say hi ...")
+}
+
+func (t *TestController) SayYes() {
+	fmt.Fprintln(t.Ctx.Output, "say yes ...")
+}
+
+func main() {
+	fw := myframework.New()
+
+	fw.AddAutoRouter("/test/", &TestController{})
+
+	fw.Run(":8999")
+
+}
